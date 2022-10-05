a4382967a97b13a6d44077808a3e6c344639b233
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-10-05 23:31:11 +0700

    Update reanima_rewrite.lua

--- reanima_rewrite.lua
+++ reanima_rewrite.lua
@@ -232,6 +232,22 @@ if rig.PrimaryPart ~= rigroot then
 	end
 end
 connections[#connections+1] = rs.Heartbeat:Connect(function()
+	local ypos = rigroot.Position.Y
+	if ypos < workspace.FallenPartsDestroyHeight / 2 then
+		for i,v in pairs(rig:GetDescendants()) do
+			if v:IsA("BasePart") then
+				v.Velocity = Vector3.new()
+				v.RotVelocity = Vector3.new()
+			end
+		end
+		rig:PivotTo(pos)
+		for i,v in pairs(rig:GetDescendants()) do
+			if v:IsA("BasePart") then
+				v.Velocity = Vector3.new()
+				v.RotVelocity = Vector3.new()
+			end
+		end
+	end
 	for i,v in pairs(alignparts) do
 		if addedaparts[i] == nil then
 			addedaparts[i] = true
